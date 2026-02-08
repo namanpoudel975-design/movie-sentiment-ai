f0831616295b71abdf29d14bd31a737683e0cb82
namanpoudel975-design <namanpoudel975@gmail.com>
2026-02-08 14:52:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from sentiment import predict_sentiment
+import os
 
 app = Flask(__name__)
 
@@ -7,13 +8,12 @@ app = Flask(__name__)
 def home():
     result = ""
     if request.method == "POST":
-        review = request.form["review"]
-        result = predict_sentiment(review)
+        review = request.form.get("review", "")
+        if review:
+            result = predict_sentiment(review)
     return render_template("index.html", result=result)
-
-if __name__ == "__main__":
-   import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
